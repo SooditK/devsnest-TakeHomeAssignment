4d6873c14a91b7d5c6863e44d7af8748ff0be30f
SooditK <sooditkumarabc@gmail.com>
2021-07-12 15:46:39 +0530

    Day19 Fixed

---
+++ Day19/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: 0,
+      num2: 0,
+      num3: 0,
+    };
+  }
+  resetnum1 = () => {
+    console.log(this.state.num1);
+    this.setState({ num1: this.state.num1 + 1 });
+    console.log(this.state.num1);
+  };
+  resetnum2 = () => {
+    this.setState({ num2: this.state.num2 + 1 });
+  };
+  resetnum3 = () => {
+    this.setState({ num3: this.state.num3 + 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="main">
+          <h1>This is a counter app</h1>
+          <div className="btn">
+            <button onClick={this.resetnum1}>{this.state.num1}</button>
+            <button onClick={this.resetnum2}>{this.state.num2}</button>
+            <button onClick={this.resetnum3}>{this.state.num3}</button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
