0d1df84005e935695a14d8d206d87ce231797295
SooditK <sooditkumarabc@gmail.com>
2021-07-12 14:42:19 +0530

    Day16 Update for Deployment

--- Day16/firstcard/src/App.js
+++ Day16/firstcard/src/App.js
@@ -1,78 +1,73 @@
-
-import './App.css';
-
-import two from "./two.png"
+import React from "react";
+import "./App.css";
+import two from "./two.png";
 
 function App() {
   return (
     <div className="App">
       <div className="Card">
         <div>
-          <img src={two} alt="" />
+          <img src={two} alt="IMAGE" />
         </div>
         <h1>My Card</h1>
         <p>This is my card</p>
       </div>
       <div className="main">
-            <div className="firstrow">
-              <div className="first"></div>
-              <div className="first"></div>
-              <div className="first"></div>
-              <div className="first"></div>
-              <div className="first"></div>
-              <div className="first"></div>
-
-            </div>
-            
-
-            <div className="secondrow">
-              <div className="second"></div>
-              <div className="second"></div>
-              <div className="second"></div>
-              <div className="second"></div>
-              <div className="second"></div>
-              <div className="second"></div>
-            </div>
-
-            <div className="thirdrow">
-              <div className="third"></div>
-              <div className="third"></div>
-              <div className="third"></div>
-              <div className="third"></div>
-              <div className="third"></div>
-              <div className="third"></div>
-            </div>
-
-            <div className="fourthrow">
-              <div className="fourth"></div>
-              <div className="fourth"></div>
-              <div className="fourth"></div>
-              <div className="fourth"></div>
-              <div className="fourth"></div>
-              <div className="fourth"></div>
-            </div>
-
-            <div className="fifthrow">
-              <div className="fifth"></div>
-              <div className="fifth"></div>
-              <div className="fifth"></div>
-              <div className="fifth"></div>
-              <div className="fifth"></div>
-              <div className="fifth"></div>
-            </div>
-
-            <div className="sixthrow">
-              <div className="sixth"></div>
-              <div className="sixth"></div>
-              <div className="sixth"></div>
-              <div className="sixth"></div>
-              <div className="sixth"></div>
-              <div className="sixth"></div>
-            </div>
+        <div className="firstrow">
+          <div className="first"></div>
+          <div className="first"></div>
+          <div className="first"></div>
+          <div className="first"></div>
+          <div className="first"></div>
+          <div className="first"></div>
         </div>
-     
+
+        <div className="secondrow">
+          <div className="second"></div>
+          <div className="second"></div>
+          <div className="second"></div>
+          <div className="second"></div>
+          <div className="second"></div>
+          <div className="second"></div>
+        </div>
+
+        <div className="thirdrow">
+          <div className="third"></div>
+          <div className="third"></div>
+          <div className="third"></div>
+          <div className="third"></div>
+          <div className="third"></div>
+          <div className="third"></div>
+        </div>
+
+        <div className="fourthrow">
+          <div className="fourth"></div>
+          <div className="fourth"></div>
+          <div className="fourth"></div>
+          <div className="fourth"></div>
+          <div className="fourth"></div>
+          <div className="fourth"></div>
+        </div>
+
+        <div className="fifthrow">
+          <div className="fifth"></div>
+          <div className="fifth"></div>
+          <div className="fifth"></div>
+          <div className="fifth"></div>
+          <div className="fifth"></div>
+          <div className="fifth"></div>
+        </div>
+
+        <div className="sixthrow">
+          <div className="sixth"></div>
+          <div className="sixth"></div>
+          <div className="sixth"></div>
+          <div className="sixth"></div>
+          <div className="sixth"></div>
+          <div className="sixth"></div>
+        </div>
+      </div>
     </div>
-   
   );
 }
 
